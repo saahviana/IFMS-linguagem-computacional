a3e5cca31c9855618be1499effeefcfc1fca5a09
Marcelo Tomporoski Perez <marcelo.perez@ifms.edu.br>
2024-03-03 10:16:38 -0400

    Modularização de Scripts

--- primeiroCod.js
+++ primeiroCod.js
@@ -1,8 +1,11 @@
-var x = 5;
-if (x <5){
-    console.log("menor que 5");
-}else if(x == 5){
-    console.log("igual a 5");
-}else{
-    console.log("maior que 5");
+// var x = 6;
+function compare(x) {
+    x=parseFloat(x);
+    if (x < 5) {
+        alert("menor que 5");
+    } else if (x == 5) {
+        alert("igual a 5");
+    } else {
+        alert("maior que 5");
+    }
 }
