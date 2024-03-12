836d9600f6deb4e34945adba9ae29917e55d5bce
Marcelo Tomporoski Perez <marcelo.perez@ifms.edu.br>
2024-03-12 09:17:34 -0400

    aula 12-03

--- js/primeiroCod.js
+++ js/primeiroCod.js
@@ -1,4 +1,9 @@
 function compare(x) {
+    // alert("-" + x + "-");
+    if (x === '') {
+        alert('O campo está vazio!');
+        return false;
+    }
     if (x < 5) {
         alert("menor que 5");
     } else if (x == 5) {
