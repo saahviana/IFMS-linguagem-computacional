9c4fd5ed6f637d6591dbb04104e4399ebe53da21
Marcelo Tomporoski Perez <marcelo.perez@ifms.edu.br>
2024-03-12 10:07:28 -0400

    Compare 5

--- js/primeiroCod.js
+++ js/primeiroCod.js
@@ -1,14 +1,18 @@
 function compare(x) {
-    // alert("-" + x + "-");
     if (x === '') {
         alert('O campo está vazio!');
         return false;
     }
     if (x < 5) {
-        alert("menor que 5");
+        if (x < 0) {
+            alert(x+" é menor que 5 e negativo");
+        } else {
+            alert(x+" é menor que 5 e positivo");
+        }
+
     } else if (x == 5) {
-        alert("igual a 5");
+        alert(x+" é igual a 5");
     } else {
-        alert("maior que 5");
+        alert(x+" é maior que 5");
     }
 }
