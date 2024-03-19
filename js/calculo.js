7b976a39dce9e148680bce9ab98bcc4b704f7738
shamara <sassa.sviana@gmail.com>
2024-03-19 11:24:18 -0300

    19-03

---
+++ js/calculo.js
@@ -0,0 +1,18 @@
+function calculo(x,y){
+    if (x==='' || y===''){
+        return('Preencha os dois campos');
+    }
+    soma=parseInt(x)+parseInt(y);
+    CalculoString = ("A Soma é igual a "+soma+"<br />");
+
+    subtrcao=x-y;
+    CalculoString += ("A subtração é igual a "+subtrcao+"<br />");
+
+    divisao=x/y;
+    CalculoString += ("A divisão é igual a "+divisao+"<br />");
+
+    multiplicacao=x*y;
+    CalculoString += ("A multiplicação é igual a "+multiplicacao);
+
+    return CalculoString
+}
--- js/primeirocodigo.js
+++ js/primeirocodigo.js
@@ -1,6 +1,6 @@
 function compare(x) {
     if (x === '') {
-        alert('O campo está vazio!');
+        alert("O campo está vazio!");
         return false;
     }
     if (x < 5) {
@@ -9,10 +9,9 @@ function compare(x) {
         } else {
             alert(x+" é menor que 5 e positivo");
         }
-
     } else if (x == 5) {
-        alert(x+" é igual a 5");
+        alert(x+" igual a 5");
     } else {
-        alert(x+" é maior que 5");
+        alert(x+" maior que 5");
     }
 }
